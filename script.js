31b927e79436808f1fb59d943cce9eee9ba8d11a
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-01-31 23:52:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.modal = modal;
+}
+Car.prototype.getMakeModel = function() {
+	return `this is ${this.make} ${this.modal}`
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	this.make = make;
+	this.modal = modal;
+	this.topSpeed = topSpeed;
+}
+Car.prototype.getTopSpeed = function() {
+	return `this is  ${this.topSpeed}`
+}
+
 
 // Do not change the code below
 window.Car = Car;
