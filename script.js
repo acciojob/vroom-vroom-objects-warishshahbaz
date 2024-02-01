9c32b2c2a70808fe0a5ff18d90c7a99c79d3a7ae
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-01 11:14:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ function Car(make, model) {
 	this.make = make;
 	this.modal = modal;
 }
-Car.prototype.getMakeModel = function() {
+Car.prototype.getMakeModel = function(){
 	return `this is ${this.make} ${this.modal}`
 }
 
-function SportsCar(make, model, topSpeed) {
-	this.make = make;
-	this.modal = modal;
+function SportsCar(make, model, topSpeed){
+	Car.call(this,make,modal);
 	this.topSpeed = topSpeed;
 }
-Car.prototype.getTopSpeed = function() {
-	return `this is  ${this.topSpeed}`
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+Car.prototype.getTopSpeed = function(){
+	return this.topSpeed
 }
 
 
